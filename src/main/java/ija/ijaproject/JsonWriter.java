9927ff873cd7f020dcb9a8b116a531f98b6bc32b
Jan Zimola <Zimolajan789@gmail.com>
2022-04-30 08:32:42 +0200

    writting reader

--- src/main/java/ija/ijaproject/JsonWriter.java
+++ src/main/java/ija/ijaproject/JsonWriter.java
@@ -3,7 +3,9 @@ package ija.ijaproject;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import ija.ijaproject.cls.ClassDiagram;
 import ija.ijaproject.cls.SequenceDiagram;
@@ -17,14 +19,51 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileWriter;
-
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsonWriter {
+
+    public boolean saveAllToFile(List<SequenceDiagram> seqDia, ClassDiagram clsDia) {
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting().serializeNulls();
+        builder.registerTypeAdapter(UMLClassInterfaceTemplate.class,
+                new UMLClassInterfaceTemplateSeriliazer());
+        builder.registerTypeAdapter(UMLClassInterfaceTemplate.class,
+                new UMLClassInterfaceTemplateDesirializer());
+        Gson gson = builder.create();
+
+        String clsString = gson.toJson(clsDia, clsDia.getClass());
+        Type sequenceListType = new TypeToken<ArrayList<SequenceDiagram>>() {
+        }.getType();
+        String sequencesString = gson.toJson(seqDia, sequenceListType);
+
+        JsonObject wholeFile = new JsonObject();
+        wholeFile.getAsJsonObject().addProperty("classDiagram", clsString);
+        wholeFile.getAsJsonObject().addProperty("sequenceDiagrams", sequencesString);
+
+        String result = wholeFile.toString();
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("examples/test1.json"));
+            writer.write(result);
+
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            // TODO: handle exception
+            return false;
+        }
+
+    }
 
     /** method processing saving class diagram */
     public boolean saveClassDiagramToFile(String pathToFile, ClassDiagram classDiagram) {
@@ -62,15 +101,11 @@ public class JsonWriter {
             builder.setPrettyPrinting().serializeNulls();
             Gson gson = builder.create();
 
-            JsonObject obj = (JsonObject) new JSONParser().parse(new FileReader(pathToFile));
-
             // UMLAttribute attr = new UMLAttribute('a', "tom", "string");
 
             String result = gson.toJson(sequenceDiagram);
 
-            JsonElement sequenceDiagrams = obj.get("sequenceDiagrams");
-
-            sequenceDiagrams.BufferedWriter writer = new BufferedWriter(new FileWriter("examples/test1.json"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("examples/test1.json"));
             writer.write(result);
 
             writer.close();
